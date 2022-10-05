982c4223db7b5c0066c0cdc026b3584754aa23f2
Yash Gupta <94608129+yashgupta0574@users.noreply.github.com>
2022-10-05 18:54:20 +0530

    Time: 166 ms (16.98%), Space: 69.8 MB (11.55%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -3,29 +3,30 @@ class Solution
     public:
         vector<vector < int>> ans;
     map<vector < int>, int> mp;
-    int target,n;
+    int target, n;
 
     void dosum(vector<int> &candidates, vector<int> member, int idx, int currsum)
     {
-        if (currsum > target || idx==n) return;
+        if (currsum > target || idx == n) return;
         if (currsum == target)
         {
-            if (mp.find(member) == mp.end())
-            {
-                ans.push_back(member);
-                mp[member]++;
-            }
+            // if (mp.find(member) == mp.end())
+            // {
+            //     ans.push_back(member);
+            //     mp[member]++;
+            // }
+            ans.push_back(member);
             return;
         }
-        dosum(candidates, member, idx + 1, currsum);
         member.push_back(candidates[idx]);
-       	dosum(candidates,member,idx,currsum+candidates[idx]);
-        dosum(candidates, member, idx + 1, currsum + candidates[idx]);
+        dosum(candidates, member, idx, currsum + candidates[idx]);
+        member.pop_back();
+        dosum(candidates, member, idx + 1, currsum);
     }
 
     vector<vector < int>> combinationSum(vector<int> &candidates, int target)
     {
-        n=candidates.size();
+        n = candidates.size();
         this->target = target;
         vector<int> member;
         dosum(candidates, member, 0, 0);
