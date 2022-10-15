619dda2849d1eed00a8190394b45d770e9099eb1
Yash Gupta <94608129+yashgupta0574@users.noreply.github.com>
2022-10-15 19:01:08 +0530

    Time: 1339 ms (25.00%), Space: 321.3 MB (9.09%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.cpp
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -1,26 +1,33 @@
 class Solution
 {
     public:
-        int dp[101][101];
-    int dfs(string &s, int left, int K)
+        int n, K;
+    vector<vector < int>> dp;
+
+    int compress(string s, int idx, int k)
     {
-        int k = K;
-        if (s.size() - left <= k) return 0;
-        if (dp[left][k] >= 0) return dp[left][k];
-        int res = k ? dfs(s, left + 1, k - 1) : 10000, c = 1;
-        for (int i = left + 1; i <= s.size(); ++i)
+        if (s.length() - idx <= k) return 0;
+        if (dp[idx][k] != -1)
         {
-            res = min(res, dfs(s, i, k) + 1 + (c >= 100 ? 3 : (c >= 10 ? 2 : (c > 1 ? 1 : 0))));
+            return dp[idx][k];
+        }
+        int K = k;
+        int res = k ? compress(s, idx + 1, k - 1) : 10000, c = 1;
+        for (int i = idx + 1; i <= s.size(); i++)
+        {
+            res = min(res, compress(s, i, k) + 1 + (c >= 100 ? 3 : (c >= 10 ? 2 : (c > 1 ? 1 : 0))));
             if (i == s.size()) break;
-            if (s[i] == s[left]) ++c;
+            if (s[i] == s[idx]) ++c;
             else if (--k < 0) break;
         }
-        return dp[left][K] = res;
+        return dp[idx][K] = res;
     }
 
     int getLengthOfOptimalCompression(string s, int k)
     {
-        memset(dp, -1, sizeof(dp));
-        return dfs(s, 0, k);
+        n = s.length();
+        this->K = k;
+        dp = vector<vector < int>> (n + 5, vector<int> (k + 5, -1));
+        return compress(s, 0, k);
     }
 };
