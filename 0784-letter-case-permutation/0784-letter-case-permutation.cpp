2d58246f57984956c21010d5bb6c73e4d139d344
Yash Gupta <94608129+yashgupta0574@users.noreply.github.com>
2022-10-17 00:18:29 +0530

    Time: 17 ms (31.08%), Space: 12.7 MB (7.39%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+        vector<string> ans;
+    int n;
+
+    void convert(string s, int idx)
+    {
+        if (idx == n)
+        {
+            ans.push_back(s);
+            return;
+        }
+        if (s[idx] >= 'a' && s[idx] <= 'z')
+        {
+            convert(s, idx + 1);
+            s[idx] -= 32;
+            convert(s, idx + 1);
+            s[idx] += 32;
+        }
+        else if (s[idx] >= 'A' && s[idx] <= 'Z')
+        {
+            convert(s, idx + 1);
+            s[idx] += 32;
+            convert(s, idx + 1);
+            s[idx] -= 32;
+        }
+        else convert(s, idx + 1);
+    }
+
+    vector<string> letterCasePermutation(string s)
+    {
+        n = s.length();
+        convert(s, 0);
+        return ans;
+    }
+};
